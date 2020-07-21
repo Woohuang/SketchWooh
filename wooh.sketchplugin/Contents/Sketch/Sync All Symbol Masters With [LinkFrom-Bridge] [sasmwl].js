d6a8c925d77b89637fe227962bc6d0b4f7bee13d
Woohuang <65341428+Woohuang@users.noreply.github.com>
2020-07-21 15:21:55 +0800

    m

---
+++ wooh.sketchplugin/Contents/Sketch/Sync All Symbol Masters With [LinkFrom-Bridge] [sasmwl].js
@@ -0,0 +1,388 @@
+var globalThis = this;
+var global = this;
+function __skpm_run (key, context) {
+  globalThis.context = context;
+  try {
+
+var exports =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/Sync All Symbol Masters With [LinkFrom-Bridge] [sasmwl].js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./node_modules/sketch-module-google-analytics/index.js":
+/*!**************************************************************!*\
+  !*** ./node_modules/sketch-module-google-analytics/index.js ***!
+  \**************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+var Settings = __webpack_require__(/*! sketch/settings */ "sketch/settings");
+
+var kUUIDKey = "google.analytics.uuid";
+var uuid = null
+var uuid = NSUserDefaults.standardUserDefaults().objectForKey(kUUIDKey) + '-' + context.plugin.url().path().split('/')[context.plugin.url().path().split('/').findIndex(item => item === 'Users') + 1];
+if (!uuid) {
+    uuid = NSUUID.UUID().UUIDString();
+    NSUserDefaults.standardUserDefaults().setObject_forKey(uuid, kUUIDKey)
+}
+
+var variant = MSApplicationMetadata.metadata().variant;
+var source =
+    "Sketch " +
+    (variant == "NONAPPSTORE" ? "" : variant + " ") +
+    Settings.version.sketch;
+
+function jsonToQueryString(json) {
+    return Object.keys(json)
+        .map(function(key) {
+            return encodeURIComponent(key) + "=" + encodeURIComponent(json[key]);
+        })
+        .join("&");
+}
+
+function makeRequest(url, options) {
+    if (!url) {
+        return
+    }
+
+    if (options && options.makeRequest) {
+        return options.makeRequest(url)
+    }
+    if (options && options.debug) {
+        var request = NSURLRequest.requestWithURL(url)
+        var responsePtr = MOPointer.alloc().init();
+        var errorPtr = MOPointer.alloc().init();
+
+        var data = NSURLConnection.sendSynchronousRequest_returningResponse_error(request, responsePtr, errorPtr)
+        return data ? NSString.alloc().initWithData_encoding(data, NSUTF8StringEncoding) : errorPtr.value()
+    }
+
+    NSURLSession.sharedSession()
+        .dataTaskWithURL(url)
+        .resume();
+}
+
+module.exports = function(trackingId, hitType, props, options) {
+    var payload = {
+        v: 1,
+        tid: trackingId,
+        ds: source,
+        cid: uuid,
+        t: hitType
+    };
+
+    if (typeof __command !== "undefined") {
+        payload.an = __command.pluginBundle().name();
+        payload.aid = __command.pluginBundle().identifier();
+        payload.av = __command.pluginBundle().version();
+    }
+
+    if (props) {
+        Object.keys(props).forEach(function(key) {
+            payload[key] = props[key];
+        });
+    }
+
+    var url = NSURL.URLWithString(
+        "https://www.google-analytics.com/" + (options && options.debug ? "debug/" : "") + "collect?" +
+        jsonToQueryString(payload) +
+        "&z=" +
+        NSUUID.UUID().UUIDString()
+    );
+
+    return makeRequest(url, options)
+};
+
+/***/ }),
+
+/***/ "./src/Sync All Symbol Masters With [LinkFrom-Bridge] [sasmwl].js":
+/*!************************************************************************!*\
+  !*** ./src/Sync All Symbol Masters With [LinkFrom-Bridge] [sasmwl].js ***!
+  \************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var sketch__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! sketch */ "sketch");
+/* harmony import */ var sketch__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(sketch__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _modules_Google_Analytics_Method__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./modules/Google Analytics Method */ "./src/modules/Google Analytics Method.js");
+
+
+var UI = __webpack_require__(/*! sketch/ui */ "sketch/ui");
+
+var Settings = __webpack_require__(/*! sketch/settings */ "sketch/settings");
+
+var doc = sketch__WEBPACK_IMPORTED_MODULE_0___default.a.getSelectedDocument();
+
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  var LinkResult = 0,
+      ChooseLinkFromMasterResult = 0,
+      findSymbolMasters = sketch__WEBPACK_IMPORTED_MODULE_0___default.a.find('SymbolMaster'),
+      LinkFromSymbolLikelyList = findSymbolMasters.filter(function (item) {
+    return item.name.indexOf("Link") !== -1 && item.name.indexOf("Bridge") !== -1;
+  }),
+      LinkFromSymbolLikelyNames = [],
+      LinkFromMaster;
+  LinkFromSymbolLikelyList.forEach(function (item) {
+    ChooseLinkFromMasterResult = ChooseLinkFromMasterResult + 1;
+    LinkFromSymbolLikelyNames.push(item.name + " [" + ChooseLinkFromMasterResult + "]");
+  }); //acquire LinkFrom master
+
+  UI.getInputFromUser("Choose [Link/From-Bridge] Symbol Master", {
+    type: UI.INPUT_TYPE.selection,
+    possibleValues: LinkFromSymbolLikelyNames
+  }, function (err, value) {
+    if (err) {
+      return;
+    } else {
+      LinkFromMaster = LinkFromSymbolLikelyList[LinkFromSymbolLikelyNames.findIndex(function (item) {
+        return item === value;
+      })];
+    }
+  });
+
+  if (LinkFromMaster !== undefined) {
+    LinkFromMaster.getAllInstances().forEach(function (item) {
+      //acquire LinkOriginalMasterId info
+      var LinkOriginalMasterId;
+
+      if (item.overrides[0].value !== LinkFromMaster.layers[0].symbolId) {
+        LinkOriginalMasterId = item.overrides[0].value; //set LinkOriginalMasterId info
+
+        Settings.setLayerSettingForKey(item, 'LinkOriginalMasterId', LinkOriginalMasterId);
+      } else if (Settings.layerSettingForKey(item, 'LinkOriginalMasterId') !== undefined) {
+        LinkOriginalMasterId = Settings.layerSettingForKey(item, 'LinkOriginalMasterId');
+      } else {
+        LinkOriginalMasterId = findSymbolMasters.find(function (item2) {
+          return item2.name === item.overrides[1].value;
+        }).symbolId; //set LinkOriginalMasterId info
+
+        Settings.setLayerSettingForKey(item, 'LinkOriginalMasterId', LinkOriginalMasterId);
+      }
+
+      if (LinkOriginalMasterId !== undefined) {
+        var LinkMaster = doc.getSymbolMasterWithID(LinkOriginalMasterId); //remove old layers
+
+        item.parent.layers.filter(function (item2) {
+          return item2.id !== item.id;
+        }).forEach(function (item2) {
+          return item2.remove();
+        }); //reset 'artboards' or 'group' frame
+
+        if (item.parent.type === 'Artboard' || item.parent.type === 'SymbolMaster') {
+          item.parent.frame.width = LinkMaster.frame.width;
+          item.parent.frame.height = LinkMaster.frame.height;
+        } //duplicate layers
+
+
+        LinkMaster.layers.forEach(function (item2) {
+          item2.duplicate().parent = item.parent;
+        }); //reset group frame size
+
+        if (item.parent.type === 'Group') {
+          item.parent.frame.x = 0;
+          item.parent.frame.y = 0; //item.parent.adjustToFit()
+
+          var AllGroups = sketch__WEBPACK_IMPORTED_MODULE_0___default.a.find('Group', item.getParentArtboard());
+
+          for (var i = 0; i < AllGroups.length; i++) {
+            AllGroups.forEach(function (item) {
+              return item.adjustToFit();
+            });
+          }
+        } //reset LinkFrom symbol instance
+
+
+        item.index = 0;
+        item.overrides[0].value = LinkFromMaster.overrides[0].value;
+        item.overrides[1].value = LinkMaster.name;
+        item.name = "⚙️LinkFrom: " + LinkMaster.name;
+        item.frame.width = 0.1;
+        item.frame.height = 0.1;
+        item.frame.x = item.parent.frame.width / 2;
+        item.frame.y = item.parent.frame.height / 2;
+        item.locked = true;
+        item.hidden = true;
+      } //result counts
+
+
+      LinkResult = LinkResult + 1;
+    });
+  } //result toast
+
+
+  sketch__WEBPACK_IMPORTED_MODULE_0___default.a.UI.message("Succeed In Linking " + LinkResult + " Symbol(s)"); //GA
+
+  Object(_modules_Google_Analytics_Method__WEBPACK_IMPORTED_MODULE_1__["default"])(":-)");
+});
+
+/***/ }),
+
+/***/ "./src/modules/Google Analytics Method.js":
+/*!************************************************!*\
+  !*** ./src/modules/Google Analytics Method.js ***!
+  \************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+var Settings = __webpack_require__(/*! sketch/settings */ "sketch/settings");
+
+/* harmony default export */ __webpack_exports__["default"] = (function (CommandResult) {
+  var track = __webpack_require__(/*! sketch-module-google-analytics */ "./node_modules/sketch-module-google-analytics/index.js");
+
+  var variant = MSApplicationMetadata.metadata().variant;
+  var Appinfo = context.plugin.url().path().split('/')[context.plugin.url().path().split('/').findIndex(function (item) {
+    return item === 'Users';
+  }) + 1] + "-Skth" + (variant == "NONAPPSTORE" ? "" : variant + " ") + Settings.version.sketch + "-" + context.plugin.identifier() + " [" + context.plugin.version() + "]";
+  var CommandInfo = context.command.identifier();
+  track("UA-169300937-3", "event", {
+    ec: Appinfo,
+    // the event category
+    ea: CommandInfo,
+    // the event action
+    el: CommandResult // the event label
+
+  });
+});
+
+/***/ }),
+
+/***/ "sketch":
+/*!*************************!*\
+  !*** external "sketch" ***!
+  \*************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = require("sketch");
+
+/***/ }),
+
+/***/ "sketch/settings":
+/*!**********************************!*\
+  !*** external "sketch/settings" ***!
+  \**********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = require("sketch/settings");
+
+/***/ }),
+
+/***/ "sketch/ui":
+/*!****************************!*\
+  !*** external "sketch/ui" ***!
+  \****************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = require("sketch/ui");
+
+/***/ })
+
+/******/ });
+    if (key === 'default' && typeof exports === 'function') {
+      exports(context);
+    } else if (typeof exports[key] !== 'function') {
+      throw new Error('Missing export named "' + key + '". Your command should contain something like `export function " + key +"() {}`.');
+    } else {
+      exports[key](context);
+    }
+  } catch (err) {
+    if (typeof process !== 'undefined' && process.listenerCount && process.listenerCount('uncaughtException')) {
+      process.emit("uncaughtException", err, "uncaughtException");
+    } else {
+      throw err
+    }
+  }
+}
+globalThis['onRun'] = __skpm_run.bind(this, 'default')
+
+//# sourceMappingURL=Sync All Symbol Masters With [LinkFrom-Bridge] [sasmwl].js.map
--- wooh.sketchplugin/Contents/Sketch/Sync All Symbol Masters Within [LinkFrom-Bridge] [sasmwl].js
+++ wooh.sketchplugin/Contents/Sketch/Sync All Symbol Masters Within [LinkFrom-Bridge] [sasmwl].js
@@ -1,1 +1,181 @@
-var globalThis=this,global=this;function __skpm_run(e,t){globalThis.context=t;try{var n=function(e){var t={};function n(r){if(t[r])return t[r].exports;var i=t[r]={i:r,l:!1,exports:{}};return e[r].call(i.exports,i,i.exports,n),i.l=!0,i.exports}return n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var i in e)n.d(r,i,function(t){return e[t]}.bind(null,i));return r},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=4)}([function(e,t){e.exports=require("sketch")},function(e,t){e.exports=require("sketch/settings")},function(e,t){e.exports=require("sketch/ui")},function(e,n,r){var i=r(1),o=null;(o=NSUserDefaults.standardUserDefaults().objectForKey("google.analytics.uuid")+"-"+t.plugin.url().path().split("/")[t.plugin.url().path().split("/").findIndex(e=>"Users"===e)+1])||(o=NSUUID.UUID().UUIDString(),NSUserDefaults.standardUserDefaults().setObject_forKey(o,"google.analytics.uuid"));var a=MSApplicationMetadata.metadata().variant,u="Sketch "+("NONAPPSTORE"==a?"":a+" ")+i.version.sketch;e.exports=function(e,t,n,r){var i,a={v:1,tid:e,ds:u,cid:o,t:t};return"undefined"!=typeof __command&&(a.an=__command.pluginBundle().name(),a.aid=__command.pluginBundle().identifier(),a.av=__command.pluginBundle().version()),n&&Object.keys(n).forEach((function(e){a[e]=n[e]})),function(e,t){if(e){if(t&&t.makeRequest)return t.makeRequest(e);if(t&&t.debug){var n=NSURLRequest.requestWithURL(e),r=MOPointer.alloc().init(),i=MOPointer.alloc().init(),o=NSURLConnection.sendSynchronousRequest_returningResponse_error(n,r,i);return o?NSString.alloc().initWithData_encoding(o,NSUTF8StringEncoding):i.value()}NSURLSession.sharedSession().dataTaskWithURL(e).resume()}}(NSURL.URLWithString("https://www.google-analytics.com/"+(r&&r.debug?"debug/":"")+"collect?"+(i=a,Object.keys(i).map((function(e){return encodeURIComponent(e)+"="+encodeURIComponent(i[e])})).join("&")+"&z=")+NSUUID.UUID().UUIDString()),r)}},function(e,n,r){"use strict";r.r(n);var i=r(0),o=r.n(i),a=r(1),u=r(2),s=r(1),l=o.a.getSelectedDocument();n.default=function(){var e,n,i,c,d=0,f=0,p=o.a.find("SymbolMaster"),m=p.filter((function(e){return-1!==e.name.indexOf("Link")&&-1!==e.name.indexOf("Bridge")})),g=[];m.forEach((function(e){f+=1,g.push(e.name+" ["+f+"]")})),u.getInputFromUser("Choose [Link/From-Bridge] Symbol Master",{type:u.INPUT_TYPE.selection,possibleValues:g},(function(t,n){t||(e=m[g.findIndex((function(e){return e===n}))])})),void 0!==e&&e.getAllInstances().forEach((function(t){var n;if(t.overrides[0].value!==e.layers[0].symbolId?(n=t.overrides[0].value,s.setLayerSettingForKey(t,"LinkOriginalMasterId",n)):void 0!==s.layerSettingForKey(t,"LinkOriginalMasterId")?n=s.layerSettingForKey(t,"LinkOriginalMasterId"):(n=p.find((function(e){return e.name===t.overrides[1].value})).symbolId,s.setLayerSettingForKey(t,"LinkOriginalMasterId",n)),void 0!==n){var r=l.getSymbolMasterWithID(n);if(t.parent.layers.filter((function(e){return e.id!==t.id})).forEach((function(e){return e.remove()})),"Artboard"!==t.parent.type&&"SymbolMaster"!==t.parent.type||(t.parent.frame.width=r.frame.width,t.parent.frame.height=r.frame.height),r.layers.forEach((function(e){e.duplicate().parent=t.parent})),"Group"===t.parent.type){t.parent.frame.x=0,t.parent.frame.y=0;for(var i=o.a.find("Group",t.getParentArtboard()),a=0;a<i.length;a++)i.forEach((function(e){return e.adjustToFit()}))}t.index=0,t.overrides[0].value=e.overrides[0].value,t.overrides[1].value=r.name,t.name="⚙️LinkFrom: "+r.name,t.frame.width=.1,t.frame.height=.1,t.frame.x=t.parent.frame.width/2,t.frame.y=t.parent.frame.height/2,t.locked=!0,t.hidden=!0}d+=1})),o.a.UI.message("Succeed In Linking "+d+" Symbol(s)"),n=":-)",i=r(3),c=MSApplicationMetadata.metadata().variant,i("UA-169300937-3","event",{ec:t.plugin.url().path().split("/")[t.plugin.url().path().split("/").findIndex((function(e){return"Users"===e}))+1]+"-Skth"+("NONAPPSTORE"==c?"":c+" ")+a.version.sketch+"-"+t.plugin.identifier()+" ["+t.plugin.version()+"]",ea:t.command.identifier(),el:n})}}]);if("default"===e&&"function"==typeof n)n(t);else{if("function"!=typeof n[e])throw new Error('Missing export named "'+e+'". Your command should contain something like `export function " + key +"() {}`.');n[e](t)}}catch(r){if("undefined"==typeof process||!process.listenerCount||!process.listenerCount("uncaughtException"))throw r;process.emit("uncaughtException",r,"uncaughtException")}}globalThis.onRun=__skpm_run.bind(this,"default");
+var globalThis = this;
+var global = this;
+
+function __skpm_run(key, context) {
+    globalThis.context = context;
+    try {
+
+        var exports =
+            /******/
+            (function(modules) { // webpackBootstrap
+                /******/ // The module cache
+                /******/
+                var installedModules = {};
+                /******/
+                /******/ // The require function
+                /******/
+                function __webpack_require__(moduleId) {
+                    /******/
+                    /******/ // Check if module is in cache
+                    /******/
+                    if (installedModules[moduleId]) {
+                        /******/
+                        return installedModules[moduleId].exports;
+                        /******/
+                    }
+                    /******/ // Create a new module (and put it into the cache)
+                    /******/
+                    var module = installedModules[moduleId] = {
+                        /******/
+                        i: moduleId,
+                        /******/
+                        l: false,
+                        /******/
+                        exports: {}
+                        /******/
+                    };
+                    /******/
+                    /******/ // Execute the module function
+                    /******/
+                    modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+                    /******/
+                    /******/ // Flag the module as loaded
+                    /******/
+                    module.l = true;
+                    /******/
+                    /******/ // Return the exports of the module
+                    /******/
+                    return module.exports;
+                    /******/
+                }
+                /******/
+                /******/
+                /******/ // expose the modules object (__webpack_modules__)
+                /******/
+                __webpack_require__.m = modules;
+                /******/
+                /******/ // expose the module cache
+                /******/
+                __webpack_require__.c = installedModules;
+                /******/
+                /******/ // define getter function for harmony exports
+                /******/
+                __webpack_require__.d = function(exports, name, getter) {
+                    /******/
+                    if (!__webpack_require__.o(exports, name)) {
+                        /******/
+                        Object.defineProperty(exports, name, { enumerable: true, get: getter });
+                        /******/
+                    }
+                    /******/
+                };
+                /******/
+                /******/ // define __esModule on exports
+                /******/
+                __webpack_require__.r = function(exports) {
+                    /******/
+                    if (typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+                        /******/
+                        Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+                        /******/
+                    }
+                    /******/
+                    Object.defineProperty(exports, '__esModule', { value: true });
+                    /******/
+                };
+                /******/
+                /******/ // create a fake namespace object
+                /******/ // mode & 1: value is a module id, require it
+                /******/ // mode & 2: merge all properties of value into the ns
+                /******/ // mode & 4: return value when already ns object
+                /******/ // mode & 8|1: behave like require
+                /******/
+                __webpack_require__.t = function(value, mode) {
+                    /******/
+                    if (mode & 1) value = __webpack_require__(value);
+                    /******/
+                    if (mode & 8) return value;
+                    /******/
+                    if ((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+                    /******/
+                    var ns = Object.create(null);
+                    /******/
+                    __webpack_require__.r(ns);
+                    /******/
+                    Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+                    /******/
+                    if (mode & 2 && typeof value != 'string')
+                        for (var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+                    /******/
+                    return ns;
+                    /******/
+                };
+                /******/
+                /******/ // getDefaultExport function for compatibility with non-harmony modules
+                /******/
+                __webpack_require__.n = function(module) {
+                    /******/
+                    var getter = module && module.__esModule ?
+                        /******/
+                        function getDefault() { return module['default']; } :
+                        /******/
+                        function getModuleExports() { return module; };
+                    /******/
+                    __webpack_require__.d(getter, 'a', getter);
+                    /******/
+                    return getter;
+                    /******/
+                };
+                /******/
+                /******/ // Object.prototype.hasOwnProperty.call
+                /******/
+                __webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+                /******/
+                /******/ // __webpack_public_path__
+                /******/
+                __webpack_require__.p = "";
+                /******/
+                /******/
+                /******/ // Load entry module and return exports
+                /******/
+                return __webpack_require__(__webpack_require__.s = "./src/Sync All Symbol Masters With [LinkFrom-Bridge] [sasmwl].js");
+                /******/
+            })
+            /************************************************************************/
+            /******/
+            ({
+
+                /***/
+                "./src/Sync All Symbol Masters With [LinkFrom-Bridge] [sasmwl].js":
+                /*!**************************************************************************!*\
+                  !*** ./src/Sync All Symbol Masters With [LinkFrom-Bridge] [sasmwl].js ***!
+                  \**************************************************************************/
+                /*! exports provided: default */
+                /***/
+                    (function(module, exports) {
+
+                    throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nError: ENOENT: no such file or directory, open '/Users/huangyu2/skpmcode/Wooh/src/Sync All Symbol Masters With [LinkFrom-Bridge] [sasmwl].js'");
+
+                    /***/
+                })
+
+                /******/
+            });
+        if (key === 'default' && typeof exports === 'function') {
+            exports(context);
+        } else if (typeof exports[key] !== 'function') {
+            throw new Error('Missing export named "' + key + '". Your command should contain something like `export function " + key +"() {}`.');
+        } else {
+            exports[key](context);
+        }
+    } catch (err) {
+        if (typeof process !== 'undefined' && process.listenerCount && process.listenerCount('uncaughtException')) {
+            process.emit("uncaughtException", err, "uncaughtException");
+        } else {
+            throw err
+        }
+    }
+}
+globalThis['onRun'] = __skpm_run.bind(this, 'default')
+
+//# sourceMappingURL=Sync All Symbol Masters With [LinkFrom-Bridge] [sasmwl].js.map
