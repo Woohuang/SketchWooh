2ee57b9cf469ee413a4e5977c875303de4dc2365
Woohuang <65341428+Woohuang@users.noreply.github.com>
2020-11-28 15:23:33 +0800

    m

--- src/Run With Sketch Runner.js
+++
@@ -1,8 +0,0 @@
-import sketch from 'sketch'
-import { GA } from "./modules/xscapeFunctions"
-
-export default function() {
-    NSWorkspace.sharedWorkspace().openURL(NSURL.URLWithString("https://sketchrunner.com/get"))
-        //GA
-    GA(":-)")
-}
