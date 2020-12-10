d23f7277a0e8e5891dcee93c41146d41585e4461
Woohuang <65341428+Woohuang@users.noreply.github.com>
2020-12-10 14:50:59 +0800

    m

--- src/Test.js
+++ src/Test.js
@@ -7,20 +7,20 @@ import BrowserWindow from 'sketch-module-web-view';
 
 export default function () {
   if (doc.pages[0].name === 'xscape') {
-    const identifier = `identifier${Date.now()}`;
+    // const identifier = `identifier${Date.now()}`;
 
-    let win = new BrowserWindow({
-      identifier,
-      width: 800,
-      height: 600,
-      alwaysOnTop: true,
-    });
-    win.on('closed', () => {
-      win = null;
-    });
+    // let win = new BrowserWindow({
+    //   identifier,
+    //   width: 800,
+    //   height: 600,
+    //   alwaysOnTop: true,
+    // });
+    // win.on('closed', () => {
+    //   win = null;
+    // });
 
-    // Or load a local HTML file
-    console.log(3333222)
-    win.loadURL(require('../resources/webview.html'));
+    // // Or load a local HTML file
+    // console.log(3333222)
+    // win.loadURL(require('../resources/webview.html'));
   }
 }
